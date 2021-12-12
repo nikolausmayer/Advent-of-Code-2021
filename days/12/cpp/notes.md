d103fc74ab5fce2cd5355bd289d90627aa221706
Nikolaus Mayer <mayern@cs.uni-freiburg.de>
2021-12-12 10:51:19 +0100

    day 12 C++

---
+++ days/12/cpp/notes.md
@@ -0,0 +1,1 @@
+This day is up there with the best of AoC for me. It requires *some* algorithmic thinking, but offers a lot of freedom in the specifics. Part 2 is conceptually straightforward, but the actual workload depends on how part 1 was solved. Runtimes are nontrivial for the larger tasks, but still suitable for fast solution iterations.
---
+++ days/12/cpp/part-1.cpp
@@ -0,0 +1,81 @@
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <stack>
+#include <set>
+#include <vector>
+
+  int main(int argc, char* argv[])
+  {
+    if (argc != 2) {
+      std::cerr << "Bad arguments" << std::endl;
+      return EXIT_FAILURE;
+    }
+    auto filename = argv[1];
+    std::ifstream infile{filename};
+    if (infile.bad() or not infile.is_open()) {
+      std::cerr << "Cannot open \"" << argv[1] << "\"" << std::endl;
+      return EXIT_FAILURE;
+    }
+    
+    std::map<std::string, std::vector<std::string>> connections;
+    std::map<std::string, bool> is_big;
+    while (true) {
+      std::string line;
+      infile >> line;
+      if (infile.eof())
+        break;
+      const auto dash_at{std::distance(std::begin(line),
+                                       std::find(std::begin(line), 
+                                                 std::end(line), 
+                                                 '-'))};
+      const auto A{line.substr(0, dash_at)};
+      const auto B{line.substr(dash_at+1)};
+      connections[A].push_back(B);
+      connections[B].push_back(A);
+      is_big[A] = (A[0] < 'a');
+      is_big[B] = (B[0] < 'a');
+    }
+
+    struct Node {
+      std::vector<std::string> path;
+    };
+    std::stack<Node> queue;
+    std::set<std::vector<std::string>> visited;
+    int result{0};
+    queue.push({{"start"}});
+    while (queue.size()) {
+      auto node = queue.top();
+      queue.pop();
+
+      if (visited.count(node.path))
+        continue;
+      visited.insert(node.path);
+
+      if (node.path.back() == "end") {
+        //std::cout << "Path found: ";
+        //for (const auto& s: node.path)
+        //  std::cout << s << ",";
+        //std::cout << std::endl;
+        ++result;
+        continue;
+      }
+
+      for (const auto& cave: connections[node.path.back()]) {
+        if (is_big[cave] or std::find(std::begin(node.path),
+                                      std::end(node.path),
+                                      cave) == std::end(node.path)) {
+          auto next{node};
+          next.path.push_back(cave);
+          queue.push(next);
+        }
+      }
+    }
+
+    std::cout << result << std::endl;
+
+    return EXIT_SUCCESS;
+  }
+
